33574db2755c8f1e0c2862b1c32b685696c3a437
happygirlzt <happygirlzt@gmail.com>
2018-06-11 12:05:55 +0800

    Test the contents

---
+++ DataStructures/LinkedList.java
@@ -0,0 +1,14 @@
+/*
+  A Linked List class with a private static inner Node class.
+*/
+
+import java.util.*;
+
+public static class LinkedList {
+    private AnyType data;
+    private Node<AnyType> next;
+
+    public static void
+
+
+}

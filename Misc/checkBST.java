f8bdf521001ff42b814182f4a2ae8695db39680a
happygirlzt <happygirlzt@gmail.com>
2018-08-20 09:57:47 +0800

    Update

---
+++ Misc/checkBST.java
@@ -0,0 +1,28 @@
+/*
+
+  A binary search tree(BST) is a node based binary tree data structure which has the following properties.
+  - The left subtree of a node contains only nodes with keys less than the node's key
+  - The right subtree of a node contains only nodes with keys greater than the node's key
+
+  - Each node(item in the tree) has a distinct key
+-- GeeksforGeeks
+
+*/
+
+public class node {
+    int val;
+    node right;
+    node left;
+
+    public node(int v) {
+        val = v;
+    }
+}
+
+public class checkBST {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ SearchingAndSorting/BinarySearch.java
@@ -0,0 +1,27 @@
+// Binary Search
+// @happygirlzt
+// Created on 19 Aug, 2018
+
+// The worstTime(n) is O(logn)
+public class BinarySearch {
+    public static int binarysearch(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;  // same effect as (low + high) / 2
+
+            int midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -(low + 1);  // not found;
+    }
+}

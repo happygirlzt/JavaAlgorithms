38be4800feefbbb96e77948aab8bf6acd8181338
happygirlzt <happygirlzt@gmail.com>
2019-06-03 17:23:43 +0800

    Add QuickSort

--- Algorithms/SearchingSorting/QuickSort.java
+++ Algorithms/SearchingSorting/QuickSort.java
@@ -24,54 +24,57 @@
  * Not stable
  */
 
-package SearchingSorting;
+// Hackerearch Link: https://www.hackerearth.com/zh/practice/algorithms/sorting/quick-sort/tutorial/
 
-import java.util.Arrays;
+import java.util.*;
 
-public class QuickSort {
-    public static void sort(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+class QuickSort {
+    private static void quickSort(int[] a, int lo, int hi) {
+        if (lo >= hi) return;
+        int pivot = partition(a, lo, hi);
+
+        quickSort(a, lo, pivot - 1);
+        quickSort(a, pivot + 1, hi);
     }
 
-    private static void quickSort(int[] nums, int lo, int hi) {
-        if (lo >= hi)
-            return;
-        int pivotIndex = partition(nums, lo, hi);
-
-        quickSort(nums, lo, pivotIndex);
-        quickSort(nums, pivotIndex + 1, hi);
-    }
-
-    private static int partition(int[] nums, int lo, int hi) {
-        // Do not forget to cast to int
-        int pivotIndex = (int) (Math.random() * (hi - lo + 1) + lo);
-        int pivot = nums[pivotIndex];
-        swap(nums, pivotIndex, hi);
-        int savedHi = hi;
+    private static int partition(int[] a, int lo, int hi) {
+        Random random = new Random();
+        int pivotIndex = random.nextInt(hi - lo + 1) + lo;
+        int pivotValue = a[pivotIndex];
+        int savedPosition = hi;
+        swap(a, hi, pivotIndex);
         hi--;
+
         while (lo <= hi) {
-            if (nums[lo] < pivot) {
-                lo++;
-            } else {
-                swap(nums, lo, hi);
+            if (a[lo] > pivotValue) {
+                swap(a, lo, hi);
                 hi--;
+            } else {
+                lo++;
             }
         }
-        swap(nums, lo, savedHi);
+
+        swap(a, lo, savedPosition);
         return lo;
     }
 
-    private static void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private static void swap(int[] a, int lo, int hi) {
+        int tmp = a[lo];
+        a[lo] = a[hi];
+        a[hi] = tmp;
     }
 
-    public static void main(String[] args) {
-        int[] x = { 1, 2, 4, 9, 3, 7, 10 };
-        System.out.println(Arrays.toString(x));
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
 
-        sort(x);
-        System.out.println(Arrays.toString(x));
+        quickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
     }
 }
